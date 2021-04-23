3be492b743c97db4ac9d5081a20c81f244dfe171
Starck43 <saloon.as@gmail.com>
2021-04-23 21:08:09 +0300

    Added Current Year function and moved developer info to gatsby-config metadata

--- src/main.js
+++ src/main.js
@@ -1,18 +1,16 @@
-
 // Social Icon
 //import instagramIcon from "./images/instagram.svg"
+
+
+export const getYear = () => {
+    return new Date().getFullYear();
+}
 
 const Settings = {
   //(Please Do Not Remove The comma(,) after every variable)
   //Change The Website Template
   // API server URL
-  serverUrl : "https://престиж-ремонт.рф/wp/wp-json/contact/v1/send",
-
-  devCompany: {
-    name: "iStarck",
-    url: "http://istarck.ru/"
-  },
-
+  serverUrl: "https://wp.престиж-ремонт.рф/wp-json/contact/v1/send",
 
   // Work Section ------------------------
   projects: [
@@ -29,11 +27,9 @@ const Settings = {
 
   // End Work Section -----------------------
 
-
-
   //   Contact Section --------------
 
-/*  contactSubHeading: "свяжитесь с нами любым удобным способом",
+  /*  contactSubHeading: "свяжитесь с нами любым удобным способом",
   social: [
     {
       img: instagramIcon, url: "https://www.instagram.com/",
